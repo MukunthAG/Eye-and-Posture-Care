353b39211b2067d2741f3bff745386b84a599142
MukunthAG <mukunthag@gmail.com>
2021-01-03 17:58:10 +0530

    Corrected some minor mistakes

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Eye & Posture Care
 A simple chrome extension which periodically reminds you to relax your Eyes and adjust your Posture. 
 
+Features Needed -
+    - Strict Mode (Closing the tab before 20 seconds results in closing of all the tabs!)
+    - Improved Design
--- popup.js
+++ popup.js
@@ -3,7 +3,7 @@ function loadTime() {
         let counter = `${time.tMin} : ${time.tSec}`;
         if (time.tMin === 20 && time.tSec <= 21) {
             document.getElementById("img").src = "2close.png";
-            document.getElementById("min20").innerHTML = "Please stop looking a the screen!"
+            document.getElementById("min20").innerHTML = "Please stop looking at the screen!"
         }
         else {
             document.getElementById("min20").innerHTML =  "Rest your Eyes in " + counter;
